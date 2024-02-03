f1bfd922a014853d704faf0ffa470ef9cb33b3b6
Karan_Kumbhar <karansunilk@iitbhilai.ac.in>
2024-02-03 13:42:22 +0530

    Test: API: main.py updated and added dummy data

--- API-Tests/main.py
+++ API-Tests/main.py
@@ -1,4 +1,7 @@
+import sys
+import csv
 import time
+import argparse
 import FoodDeliveryAPI
 from FoodDeliveryAPI.rest import ApiException
 from pprint import pprint
@@ -8,71 +11,103 @@ from FoodDeliveryAPI.models.login_request import LoginRequest
 from FoodDeliveryAPI.models.vendor_add_product_request import VendorAddProductRequest
 
 
-configuration = FoodDeliveryAPI.Configuration(
-    host = "http://13.233.99.87:8080"
-)
+def read_menu_items(file_path):
+    items = []
+    with open(file_path, 'r', newline='') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            items.append({
+                "name": row['name'],
+                "maxQuantity": int(row['maxQuantity']),
+                "price": float(row['price'])
+            })
+    return items
 
 
-# End to End test for vendor flow
-with FoodDeliveryAPI.ApiClient(configuration) as api_client:
-    # Create an instance of the API class
-    api_instance = FoodDeliveryAPI.DefaultApi(api_client)
-    id = 'id_example'
-    count = 56
-    session_id = ''
+def addVendorProduct(host):
+    configuration = FoodDeliveryAPI.Configuration(
+        host = host
+    )
+    
+    # End to End test for vendor flow
+    with FoodDeliveryAPI.ApiClient(configuration) as api_client:
+        # Create an instance of the API class
+        api_instance = FoodDeliveryAPI.DefaultApi(api_client)
+        id = 'id_example'
+        menu_file_path = "./API-Tests/dummy_data/food_item.csv"
+        count = 56
+        session_id = ''
 
-    try:
-        # Check Availability
-        print("Regestering Vendor...")
-        api_response = api_instance.register_vendor(VendorDetails.from_dict(
-            {"username": "vendor001",
-             "password": "1234@Abcd",
-             "name": "UltraTech Cafe",
-             "phone": "9876214705",
-             "email": "mymail@main.main",
-             "location": "MessBlock"}))
-
-        print("Session Id is:\n")
-        session_id = str.strip(api_response)
-        print(session_id, len(session_id))
-    except ApiException as e:
-        print("Exception when calling register_vendor: %s\n" % e)
-        # Check the reason
-        # if register failed, login
         try:
-            print("Registeration Failed! Vendor already exists, Logging in...")
-            api_response = api_instance.login_vendor(LoginRequest.from_dict({
-                "username": "vendor001",
-                "password": "1234@Abcd"
-            }))
+            # Check Availability
+            print("Regestering Vendor...")
+            api_response = api_instance.register_vendor(VendorDetails.from_dict(
+                {"username": "vendor001",
+                 "password": "1234@Abcd",
+                 "name": "UltraTech Cafe",
+                 "phone": "9876214705",
+                 "email": "mymail@main.main",
+                 "location": "MessBlock"}))
 
             print("Session Id is:\n")
             session_id = str.strip(api_response)
             print(session_id, len(session_id))
         except ApiException as e:
-            print("Exception when calling login_vendor: %s\n" % e)
+            print("Exception when calling register_vendor: %s\n" % e)
+            # Check the reason
+            # if register failed, login
+            try:
+                print("Registeration Failed! Vendor already exists, Logging in...")
+                api_response = api_instance.login_vendor(LoginRequest.from_dict({
+                    "username": "vendor001",
+                    "password": "1234@Abcd"
+                }))
 
-    items = [
-        {"name": 'Pizza Margherita', "maxQunatity": 9, "price": 50},
-        {"name": 'Chicken Caesar Salad', "maxQunatity": 7, "price": 30},
-        {"name": 'Spaghetti Bolognese', "maxQunatity": 12, "price": 40},
-        {"name": 'Grilled Salmon', "maxQunatity": 15, "price": 20},
-        {"name": 'Cheeseburger', "maxQunatity": 6, "price": 50},
-        {"name": 'Mushroom Risotto', "maxQunatity": 10, "price": 35},
-        {"name": 'Chicken Tandoori', "maxQunatity": 13, "price": 30},
-        {"name": 'Caesar Wrap', "maxQunatity": 7, "price": 40},
-        {"name": 'Chocolate Fondue', "maxQunatity": 14, "price": 15},
-        {"name": 'Shrimp Scampi', "maxQunatity": 17, "price": 25},
-        {"name": 'Vegetable Stir Fry', "maxQunatity": 9, "price": 35},
-        {"name": 'Caprese Salad', "maxQunatity": 8, "price": 30},
-        {"name": 'BBQ Ribs', "maxQunatity": 19, "price": 20},
-        {"name": 'Pad Thai', "maxQunatity": 11, "price": 40}
-    ]
+                print("Session Id is:\n")
+                session_id = str.strip(api_response)
+                print(session_id, len(session_id))
+            except ApiException as e:
+                print("Exception when calling login_vendor: %s\n" % e)
 
-    print(f"Registering products with session id {session_id} (len: {len(session_id)})")
-    for item in items:
-        try:
-            api_response = api_instance.vendor_add_product(session_id, VendorAddProductRequest.from_dict(item))
-            print(f"Added product {item['name']}, product id is {api_response.strip()}")
-        except ApiException as e:
-            print("Exception when calling login_vendor: %s\n" % e)
+        items = read_menu_items(menu_file_path)
+
+        print(f"Registering products with session id {session_id} (len: {len(session_id)})")
+        for item in items:
+            try:
+                api_response = api_instance.vendor_add_product(session_id, VendorAddProductRequest.from_dict(item))
+                print(f"Added product {item['name']}, product id is {api_response.strip()}")
+            except ApiException as e:
+                print("Exception when calling login_vendor: %s\n" % e)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Add the data to server with server specifications")
+    
+    parser.add_argument('-a', '--address', default="13.233.99.87", help='Specify the IP address')
+    parser.add_argument('-p', '--port', default="8080", help='Specify the port')
+    parser.add_argument('-H', '--host', default="http://13.233.99.87:8080", help='Specify the host URL')
+
+    args = parser.parse_args()
+
+    ip_address = args.address
+    port = args.port
+    host = args.host
+
+    if ip_address is not None and port is not None:
+        host = f"http://{ip_address}:{port}"
+    elif  ip_address is not None:  
+        host = f"http://{ip_address}:8080"
+    elif port is not None:
+        host = f"http://13.233.99.87:{port}"
+    else:
+        print("Error: If host URL is not specified, both IP address and port are required.")
+        parser.print_help()
+        return
+
+    print(f"Using host URL: {host}")
+
+    # host = "http://13.233.99.87:8080"
+    addVendorProduct(host)
+
+if __name__ == "__main__":
+    main()
